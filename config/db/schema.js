4a8ec915d3fde9db436fdd4d2fa193d86bad584d
Bharath <bharathj0410@gmail.com>
2023-08-15 13:46:57 +0530

    Update schema.js

--- config/db/schema.js
+++ config/db/schema.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
   firstname: { type: String, required: true },
   lastname: { type: String, required: true },
-  username: { type: String, required: true , unique:true},
+  username: { type: String, required: true },
   emailid: { type: String, required: true },
   phoneno: { type: Number, required: true },
   password: { type: String, required: true },
