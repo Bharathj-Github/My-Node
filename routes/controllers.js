4bd09417830103296242076c9cf2c60e60d47183
Bharath <bharathj0410@gmail.com>
2023-08-21 14:15:05 +0530

    ok

--- routes/controllers.js
+++ routes/controllers.js
@@ -11,27 +11,28 @@ exports.login = async (req, res) => {
   const password = req.body.password;
 
   if (!username) {
-    res.status(206).json({ message: "UserName Must provide" });
+    res.status(206).json({ message: "UserName Must Be Provided !" });
   } else {
     let user = await model.findOne({ username });
     if (!password) {
-      if (user === null) {
-        res.status(206).json({ message: "userName is not in the database" });
-      }else {
-        res.status(206).json({ message: "Password must be provided" });
-      }
+      res.status(206).json({ message: "Password Must Be Provided !" });
     } else {
-      bcrypt.compare(password,user.password,(err,result)=>{
-        if(result){
-          const user_id = {userid :user._id}
-          const accessToken = jwt.sign(user_id, process.env.ACCESS_TOKEN_SECRET,{expiresIn: "30s"});
-          res.cookie('jwt', `${accessToken}`,{expiresIn: '30s'});
-          res.status(200).json({message: "success",accessToken});
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          const user_id = { userid: user._id };
+          const accessToken = jwt.sign(
+            user_id,
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "30s" }
+          );
+          res.cookie("jwt", `${accessToken}`, { expiresIn: "30s" });
+          res.status(200).json({ message: "success", accessToken });
+        } else {
+          res
+            .status(206)
+            .json({ message: "UserName And Password Doesn't Match !" });
         }
-        else{
-          res.status(206).json({message: "username and password doesn't match"})
-        }
-      })
+      });
     }
   }
 };
